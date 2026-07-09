0d530a1e4ce85237fa3e270508d5f1888756ab75
nishant18084 <nishantkumar749204@gmail.com>
2026-07-09 18:18:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
-// GEMINI TRAIN TRACKER CORE CONFIGURATION
+// GEMINI AI CORE CONFIGURATION
 const GEMINI_CONFIG = {
-    API_KEY: "zwutBeqvqAQLu8NYCCuLYVkA-dW38D2WI70NVdsAFA
+    API_KEY: "zwutBeqvqAQLu8NYCCuLYVkA-dW38D2WI70NVdsAFA", // Aapki provide ki gayi Gemini API key
+    MODEL_URL: "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent"
 };
